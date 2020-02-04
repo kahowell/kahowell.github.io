3d263fc80c9c014347f6180d06f84fef2a5f3c02
Kevin Howell <kevin@kahowell.net>
2020-02-03 20:29:17 -0500

    Add nutrition PWA experiment

--- projects/index.markdown
+++ projects/index.markdown
@@ -6,6 +6,7 @@ active: projects
 # projects
 
 <ul class="posts">
+    <li><a href="nutrition">Nutrition w/ Pyodide</a> (<a href="https://github.com/kahowell/nutrition">Code</a>)</li>
     <li><a href="ps1-memory-card-editor">PS1 Memory Card Editor</a></li>
     {% for post in site.categories.project %}
         <li><span><a href="{{ post.url }}">{{ post.title }}</a></span></li>
---
+++ projects/nutrition/pyodide_config.js
@@ -0,0 +1,98 @@
+self.languagePluginUrl = './';
+pyodide_packages = [
+    
+];
+
+pyodideapp_www_files = [
+    
+    './fetchimport.py',
+    
+    './sw.js',
+    
+    './httpimport.py',
+    
+    './bootstrap.js',
+    
+    './packages.json',
+    
+    './pyodide.asm.js',
+    
+    './pyodide.asm.data',
+    
+    './pyodide.asm.data.js',
+    
+    './pyodide.asm.wasm',
+    
+    './pyodide.js',
+    
+    './all_foods.json',
+    
+    './pouchdb.find.min.js',
+    
+    './pouchdb.min.js',
+    
+    './onsenui.min.css',
+    
+    './onsen-css-components.min.css',
+    
+    './onsenui.min.js',
+    
+    './vue-onsenui.min.js',
+    
+    './vue.min.js',
+    
+    './1F373_192.png',
+    
+    './1F373_512.png',
+    
+    './1F373.svg',
+    
+    './1F336.svg',
+    
+    './1F34A.svg',
+    
+    './1F957.svg',
+    
+    './groceries.py',
+    
+    './plan.py',
+    
+    './vue.py',
+    
+    './recipe.py',
+    
+    './nutrition.webmanifest',
+    
+    './main.py',
+    
+    './recommendations.py',
+    
+    './store.py',
+    
+    './index.html',
+    
+    './font_awesome/webfonts/fa-solid-900.ttf',
+    
+    './font_awesome/webfonts/fa-solid-900.woff',
+    
+    './font_awesome/webfonts/fa-solid-900.woff2',
+    
+    './font_awesome/webfonts/fa-brands-400.woff',
+    
+    './font_awesome/webfonts/fa-brands-400.woff2',
+    
+    './font_awesome/css/v4-shims.min.css',
+    
+    './font_awesome/css/all.min.css',
+    
+    './material-design-iconic-font/fonts/Material-Design-Iconic-Font.ttf',
+    
+    './material-design-iconic-font/fonts/Material-Design-Iconic-Font.woff2',
+    
+    './material-design-iconic-font/fonts/Material-Design-Iconic-Font.woff',
+    
+    './material-design-iconic-font/css/material-design-iconic-font.min.css',
+    
+    './ionicons/css/ionicons.min.css',
+    
+];
